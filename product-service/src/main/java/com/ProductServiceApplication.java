fb3d80dcf130bac36b221ca957d6ecd8523f5a7e
omarhammouda0 <103590310+omarhammouda0@users.noreply.github.com>
2026-03-18 23:25:33 +0100

    feat: add distributed tracing with Zipkin and Micrometer

--- product-service/src/main/java/com/ProductServiceApplication.java
+++ product-service/src/main/java/com/ProductServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.resilience.annotation.EnableResilientMethods;
+
 
 
 @SpringBootApplication
